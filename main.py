66aae0c2a86dc238178dd019a6aa75e32bcbc219
Harold <harold.vilander@gmail.com>
2025-01-01 15:15:54 -0600

    moveable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -9,6 +10,12 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clk = pygame.time.Clock()
+    dt = 0
+
+    px = SCREEN_WIDTH / 2
+    py = SCREEN_HEIGHT / 2
+    p = Player(px, py)
 
     while True:
         screen.fill((0,0,0))
@@ -18,9 +25,12 @@ def main():
                 return
 
 
+        p.update(dt)
+        p.draw(screen)        
 
 
         pygame.display.flip()
+        dt = clk.tick(60) / 1000
 
 
 
